03d90c7efae6aec31dfa2e51f4c793336bd5edbf
joshua allen pearson <joshapearson@gmail.com>
2016-12-05 09:09:26 -0600

    Add Foreach Method to Iterate through items
    Changed display method from DisplayFor to Foreach, to take advanage of
    generating through array

--- webBowlingProject/Controllers/HomeController.cs
+++ webBowlingProject/Controllers/HomeController.cs
@@ -4,29 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 using webBowlingProject.Models;
 using webBowlingProject.ViewModels;
 
 namespace webBowlingProject.Controllers
 	{
-	public class HomeController : Controller
-		{
-		public ActionResult Index(GamesViewModel model)
-			{
-			Random rnd = new Random();
-            int roll = rnd.Next(1, 10);         
-                Games game = new Games()
-                {
-                    Pins = roll,
-                    GameId = model.GameId,
-                    frames = model.frames,
-                    Score = model.Score,
-                    player = model.player,
-                };
-         
-            return View("Index", model);
-			}
-            public JsonResult getFrame(int GameId)
+
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index(GamesViewModel model)
+        {
+            Random rnd = new Random();
+            int roll = rnd.Next(1, 10);
+            int[] Frames = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var vm = new List<Games>
+            {
+                new Games {frames = Frames[0], Pins = roll, Score = model.Pins  }
+            };
+            return View (vm);
+        }
+
+        public JsonResult getFrame(int GameId)
         {
 			Games games = new Games();
 			gamesAjax i = new gamesAjax { Pins = games.Pins, frames = games.frames, score = games.Score };
@@ -45,9 +45,20 @@ namespace webBowlingProject.Controllers
 
 
 
+//Random rnd = new Random();
+//         int roll = rnd.Next(1, 10);
+
+//             Games game = new Games()
+//             {
+//                 Pins = roll,
+//                 GameId = model.GameId,
+//                 frames = model.frames,
+//                 Score = model.Score,
+//                 player = model.player,
+//             };
+//         return View("Index", model);
 
 
 
 
 
-   
